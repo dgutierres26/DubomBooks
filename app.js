f1b9dd6a1b2e09ffa1c0e7530bb88ddac5ebe398
dgutierres26 <diegogutierres@hotmail.com>
2021-01-02 18:37:59 +0000

    Add items and delete

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ xsltProcess = require('xslt-processor').xsltProcess; //The same module allows us
 xml2js = require('xml2js'); //This module does XML to JSON conversion and also allows us to get from JSON back to XML
 
 app.use(express.static(path.resolve(__dirname, 'views'))); //We define the views folder as the one where all static content will be served
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json()); //We include support for JSON that is coming from the client
 
 app.use((req, res, next) => {
@@ -17,20 +17,20 @@ app.use((req, res, next) => {
 
 // Function to read in XML file and convert it to JSON
 function xmlFileToJs(filename, cb) {
-  var filepath = path.normalize(path.join(__dirname, filename));
-  fs.readFile(filepath, 'utf8', function(err, xmlStr) {
-    if (err) throw (err);
-    xml2js.parseString(xmlStr, {}, cb);
-  });
+    var filepath = path.normalize(path.join(__dirname, filename));
+    fs.readFile(filepath, 'utf8', function (err, xmlStr) {
+        if (err) throw (err);
+        xml2js.parseString(xmlStr, {}, cb);
+    });
 }
 
 //Function to convert JSON to XML and save it
 function jsToXmlFile(filename, obj, cb) {
-  var filepath = path.normalize(path.join(__dirname, filename));
-  var builder = new xml2js.Builder();
-  var xml = builder.buildObject(obj);
-  fs.unlinkSync(filepath);
-  fs.writeFile(filepath, xml, cb);
+    var filepath = path.normalize(path.join(__dirname, filename));
+    var builder = new xml2js.Builder();
+    var xml = builder.buildObject(obj);
+    fs.unlinkSync(filepath);
+    fs.writeFile(filepath, xml, cb);
 }
 
 // It's redenring the file index
@@ -39,16 +39,73 @@ app.get('/', (req, res) => {
 });
 
 app.get('/get/html', (req, res) => {
-    
-    res.writeHead(200, {'Content-Type': 'text/html'}); //We are responding to the client that the content served back is HTML and the it exists (code 200)
-    var xml = fs.readFileSync('dubombooks.xml', 'utf8'); //We are reading in the XML file
-    var xsl = fs.readFileSync('dubombooks.xsl', 'utf8'); //We are reading in the XSL file
+    //We are responding to the client that the content served back is HTML and the it exists (code 200)
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    //We are reading in the XML file 
+    var xml = fs.readFileSync('dubombooks.xml', 'utf8');
+    //We are reading in the XSL file
 
-    var doc = xmlParse(xml); //Parsing our XML file
-    var stylesheet = xmlParse(xsl); //Parsing our XSL file
+    var xsl = fs.readFileSync('dubombooks.xsl', 'utf8');
+    //Parsing our XML file
+    var doc = xmlParse(xml);
+    //Parsing our XSL file
+    var stylesheet = xmlParse(xsl);
+    //This does our XSL Transformation 
+    var result = xsltProcess(doc, stylesheet);
+    //Send the result back to the user, but convert to type string first 
+    res.end(result.toString());
+});
 
-    var result = xsltProcess(doc, stylesheet); //This does our XSL Transformation
-    res.end(result.toString()); //Send the result back to the user, but convert to type string first
+// Add new items
+app.post('/post/json', function (req, res) {
+
+    function appendJSON(obj) {
+
+        console.log(obj)
+
+        xmlFileToJs('dubombooks.xml', function (err, result) {
+            if (err) throw (err);
+
+            result.books.section[obj.sec_n].book.push({ 'item': obj.item, 'price': obj.price });
+
+            console.log(JSON.stringify(result, null, "  "));
+
+            jsToXmlFile('PaddysCafe.xml', result, function (err) {
+                if (err) console.log(err);
+            });
+        });
+    };
+
+    appendJSON(req.body);
+
+    res.redirect('back');
+
+});
+
+// Delete items
+app.post('/post/delete', function (req, res) {
+
+    function deleteJSON(obj) {
+
+        console.log(obj)
+
+        xmlFileToJs('dubombooks.xml', function (err, result) {
+            if (err) throw (err);
+
+            delete result.books.section[obj.section].book[obj.entree];
+
+            console.log(JSON.stringify(result, null, "  "));
+
+            jsToXmlFile('dubombooks.xml', result, function (err) {
+                if (err) console.log(err);
+            });
+        });
+    };
+
+    deleteJSON(req.body);
+
+    res.redirect('back');
+
 });
 
 app.listen(3000, () => {
